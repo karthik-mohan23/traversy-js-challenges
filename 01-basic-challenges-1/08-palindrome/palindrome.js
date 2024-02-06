a35340149e71f5022694d6d671b3b7f9528d9fd6
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-07 00:01:42 +0530

    01-08-palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,25 @@
-function isPalindrome() {}
+function ignore(str) {
+  let newString = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " " || str[i] === ",") {
+      continue;
+    }
+    newString += str[i];
+  }
+  return newString.toLowerCase();
+}
+
+function reverseString(str) {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
+}
+
+function isPalindrome(str) {
+  let sanitizedString = ignore(str);
+  return sanitizedString === reverseString(sanitizedString);
+}
 
 module.exports = isPalindrome;
