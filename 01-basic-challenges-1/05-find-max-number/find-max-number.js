b1ac13e2573ccf4060a814445a4579584331ce8a
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-06 23:19:43 +0530

    01-05-findMaxNumber

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,13 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let maxNumber = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxNumber) {
+      maxNumber = arr[i];
+    }
+  }
+
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
