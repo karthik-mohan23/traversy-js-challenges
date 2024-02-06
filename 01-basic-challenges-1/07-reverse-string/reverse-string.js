8ef062bb0cbceb4b28fa77b1284eea4fd5b50c7f
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-06 23:39:28 +0530

    01-07-reverseString

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,21 @@
-function reverseString() {}
+// function reverseString(str) {
+//   const storeArr = [];
+//   let index = 0;
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     storeArr[index] = str[i];
+//     ++index;
+//   }
+
+//   return storeArr.join("");
+// }
+function reverseString(str) {
+  let reversedString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+
+  return reversedString;
+}
 
 module.exports = reverseString;
