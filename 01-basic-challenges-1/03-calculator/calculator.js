682ecb4c4712b5bc50ec3af2a6d899ae61a462ad
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-06 22:47:33 +0530

    completed calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,18 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (operator === "+") {
+    return num1 + num2;
+  }
+  if (operator === "-") {
+    return num1 - num2;
+  }
+  if (operator === "*") {
+    return num1 * num2;
+  }
+  if (operator === "/") {
+    return num1 / num2;
+  }
+
+  return -1;
+}
 
 module.exports = calculator;
