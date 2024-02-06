9f0a2754e8d551a95b671d612d67de0234d40084
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-06 23:30:33 +0530

    01-06-titleCase

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,12 @@
-function titleCase() {}
+function titleCase(str) {
+  const stringArray = str.split(" ");
+
+  for (let i = 0; i < stringArray.length; i++) {
+    stringArray[i] =
+      stringArray[i].charAt(0).toUpperCase() + stringArray[i].substr(1);
+  }
+
+  return stringArray.join(" ");
+}
 
 module.exports = titleCase;
