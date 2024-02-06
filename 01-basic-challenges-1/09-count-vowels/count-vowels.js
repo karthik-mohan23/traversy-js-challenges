528683dd09d8e2b073eeee6bae3925090d3ec98d
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-07 00:10:03 +0530

    01-09-countVowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(str) {
+  const vowelsArray = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowelsArray.includes(str.toLowerCase()[i])) {
+      ++count;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
