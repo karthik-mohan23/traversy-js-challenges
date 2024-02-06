177e9cdab9105384c527b9eec227186cf6c7b177
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-07 00:17:09 +0530

    01-10-removeDuplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,15 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  const removeDuplicatesArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (removeDuplicatesArray.includes(arr[i])) {
+      continue;
+    } else {
+      removeDuplicatesArray.push(arr[i]);
+    }
+  }
+
+  return removeDuplicatesArray;
+}
 
 module.exports = removeDuplicates;
