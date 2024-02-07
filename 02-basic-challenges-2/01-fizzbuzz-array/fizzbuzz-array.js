505c72951e5e9483af2b0cfc8e7e22c1727b9f98
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-07 15:46:15 +0530

    FizzBuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(n) {
+  let newArray = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      newArray.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      newArray.push("Fizz");
+    } else if (i % 5 === 0) {
+      newArray.push("Buzz");
+    } else {
+      newArray.push(i);
+    }
+  }
+
+  return newArray;
+}
 
 module.exports = fizzBuzzArray;
