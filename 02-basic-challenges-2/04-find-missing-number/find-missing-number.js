55ef258e0c2e46a9401350218496f035518c2c7f
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-12 10:06:03 +0530

    find-missing-number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,22 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (!arr) {
+    return undefined;
+  }
+
+  let arrLength = arr.length;
+
+  if (arrLength === 0) {
+    return 1;
+  }
+
+  const arrSum1 = ((arrLength + 1) * (arrLength + 1 + 1)) / 2;
+
+  let arrSum2 = 0;
+  for (let i = 0; i < arrLength; i++) {
+    arrSum2 += arr[i];
+  }
+
+  return arrSum1 - arrSum2;
+}
 
 module.exports = findMissingNumber;
