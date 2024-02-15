646f381c05622109be3205888b91bfad174a256e
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-15 10:37:52 +0530

    are-all-chars-unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,17 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const holdStr = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (holdStr[char]) {
+      return false;
+    } else {
+      holdStr[char] = true;
+    }
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
