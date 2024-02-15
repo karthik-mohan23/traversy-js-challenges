98faa8d9fda5280ed443cf6fef4148e2fcfe4c7a
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-15 10:28:41 +0530

    find missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
+function findMissingLetter(strArr) {
+  let testString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  const startingIndexInTestString = testString.indexOf(strArr[0]);
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] !== testString[startingIndexInTestString + i]) {
+      return testString[startingIndexInTestString + i];
+    }
+  }
+
+  return "";
+}
 
 module.exports = findMissingLetter;
