207293f78632eb16698a20f819b00550eb1262ee
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-16 10:36:07 +0530

    first-non-repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,21 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(strArg) {
+  let store = {};
+
+  for (let i = 0; i < strArg.length; i++) {
+    if (store[strArg[i]]) {
+      store[strArg[i]] += 1;
+    } else {
+      store[strArg[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < strArg.length; i++) {
+    if (store[strArg[i]] === 1) {
+      return strArg[i];
+    }
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
