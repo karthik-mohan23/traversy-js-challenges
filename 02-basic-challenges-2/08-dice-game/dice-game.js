d0cf4e65a1f5c877fcf58d803be964605a14ab9d
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-16 11:13:16 +0530

    dice-game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,27 @@
-function diceGameSimulation() {}
+function dicerollNumGenerator() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function diceGameSimulation(numSimulations) {
+  let finalResults = [];
+
+  for (let i = 0; i < numSimulations; i++) {
+    let dice1 = dicerollNumGenerator();
+    let dice2 = dicerollNumGenerator();
+    let result = "";
+    let sum = dice1 + dice2;
+    if (sum === 7 || sum === 11) {
+      result = "win";
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = "lose";
+    } else {
+      result = "roll again";
+    }
+
+    finalResults.push({ dice1, dice2, sum, result });
+  }
+
+  return finalResults;
+}
 
 module.exports = diceGameSimulation;
