0122a6b5824f93d19cc4a7101a4195bc2ece6b12
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-17 09:56:07 +0530

    format-phone-number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,7 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arrNum) {
+  return `(${arrNum.slice(0, 3).join("")}) ${arrNum
+    .slice(3, 6)
+    .join("")}-${arrNum.slice(6).join("")}`;
+}
 
 module.exports = formatPhoneNumber;
