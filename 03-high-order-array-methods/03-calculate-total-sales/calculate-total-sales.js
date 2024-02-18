e31cdab3f0193b46a9dbf0f6a328e0f6eb6543fc
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-18 10:30:24 +0530

    total sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(productArr) {
+  const price = productArr.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  const totalPrice = price + (price * 8) / 100;
+
+  return totalPrice;
+}
 
 module.exports = calculateTotalSalesWithTax;
