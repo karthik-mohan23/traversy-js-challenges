8257320618726c037ca40c3fb3a43f366631edee
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-18 10:17:59 +0530

    sumOfEvenSquares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  const evenNumArr = arr.filter((item) => item % 2 === 0);
+
+  const squareNumsArr = evenNumArr.map((item) => item ** 2);
+
+  const totalSum = squareNumsArr.reduce((acc, item) => acc + item, 0);
+
+  return totalSum;
+}
 
 module.exports = sumOfEvenSquares;
