cf3a6e2f96b815ff9b194f407130c8f5de4390ee
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-19 15:22:54 +0530

    highest-scoring-word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,47 @@
-function highestScoringWord() {}
+function highestScoringWord(testStr) {
+  const arrStr = testStr.split(" ");
+
+  // create array for scores
+  const scoresArr = arrStr.map((str) => {
+    let score = 0;
+    for (let i = 0; i < str.length; i++) {
+      score += str[i].charCodeAt() - 96;
+    }
+    return score;
+  });
+  const maxValue = Math.max(...scoresArr);
+  const maxIndex = scoresArr.indexOf(maxValue);
+
+  return arrStr[maxIndex];
+}
+
+// function highestScoringWord(testStr) {
+//   const arrStr = testStr.split(" ");
+
+//   const obj = {};
+//   let score = 0;
+//   let storeScore = 0;
+//   for (let i = 0; i < arrStr.length; i++) {
+//     storeScore = 0;
+//     const str = arrStr[i];
+
+//     for (let j = 0; j < str.length; j++) {
+//       if (obj[str]) {
+//         continue;
+//       } else {
+//         let letter = str[j];
+//         let charCode = letter.charCodeAt() - 96;
+//         storeScore += charCode;
+//       }
+//     }
+
+//     if (storeScore > score) {
+//       score = storeScore;
+//     }
+//     obj[str] = storeScore;
+//   }
+
+//   return Object.keys(obj).reduce((a, b) => (obj[a] > obj[b] ? a : b));
+// }
 
 module.exports = highestScoringWord;
