4142565be4f8334019a22f4b9929ec8dbc51394f
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-20 11:03:03 +0530

    hashtag generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,23 @@
-function generateHashtag() {}
+function generateHashtag(strParam) {
+  let sanitizeString = strParam.trim();
+
+  if (!sanitizeString) {
+    return false;
+  }
+
+  //   It will split the string wherever there are one or more whitespace characters
+  let arr = sanitizeString.split(/\s+/);
+
+  let capiataliseFirstLetter = arr.map(
+    (item) => item[0].toUpperCase() + item.slice(1)
+  );
+
+  let addHashtag = "#" + capiataliseFirstLetter.join("");
+
+  if (addHashtag.length > 140) {
+    return false;
+  }
+  return addHashtag;
+}
 
 module.exports = generateHashtag;
