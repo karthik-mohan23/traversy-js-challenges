1874437568e2df3097709843bfde031401106707
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-20 10:00:28 +0530

    valid-anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,26 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  let obj1 = {};
+  let obj2 = {};
+
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (obj1[arr1[i]]) {
+      obj1[arr1[i]] += 1;
+    } else {
+      obj1[arr1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    if (obj2[arr2[i]]) {
+      obj2[arr2[i]] += 1;
+    } else {
+      obj2[arr2[i]] = 1;
+    }
+  }
+
+  return Object.keys(obj1).every((char) => obj1[char] === obj2[char]);
+}
 
 module.exports = validAnagrams;
