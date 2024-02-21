3cf0b36b1f620cecd27cdf5f3da2d31fecb89777
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-21 09:55:42 +0530

    validIp

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,14 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ipString) => {
+  const ipArray = ipString.split(".");
+  if (ipArray.length !== 4) {
+    return false;
+  }
+
+  const isValidIp = ipArray.every((ip) => {
+    const numberIp = parseInt(ip);
+    return numberIp > 0 && numberIp <= 255 && numberIp.toString() === ip;
+  });
+  return isValidIp;
+};
 
 module.exports = isValidIPv4;
