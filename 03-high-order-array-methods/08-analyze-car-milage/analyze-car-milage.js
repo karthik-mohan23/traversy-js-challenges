7f39d90bd172efc75b847ea6e2f905ec72e4b8fa
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-21 10:36:40 +0530

    analyze-car-mileage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,25 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((initial, current) => {
+    return initial + current.mileage;
+  }, 0);
+  const averageMileage = Number((totalMileage / cars.length).toFixed(2));
+  const highestMileageCar = cars.reduce((initial, current) => {
+    if (current.mileage > initial.mileage) {
+      return current;
+    } else {
+      return initial;
+    }
+  }, cars[0]);
+  const lowestMileageCar = cars.reduce((initial, current) => {
+    return initial.mileage < current.mileage ? initial : current;
+  }, cars[0]);
+
+  return {
+    totalMileage,
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+  };
+}
 
 module.exports = analyzeCarMileage;
