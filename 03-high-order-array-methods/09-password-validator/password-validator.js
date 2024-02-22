b617e9649be71746393831dc81dc41440bf80f6d
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-02-22 10:08:48 +0530

    password-validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,22 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const passwordArray = password.split("");
+
+  const hasValidLength = passwordArray.length >= 8;
+
+  //   doing char === char.toUpperCase() for "2" gives you true (indicating there's an uppercase letter when there's a number) even though it shouldn't.
+  const hasUpperCaseLetter = passwordArray.some(
+    (letter) =>
+      letter === letter.toUpperCase() && !Number.isInteger(parseInt(letter))
+  );
+
+  const hasLowerCaseLetter = passwordArray.some(
+    (letter) =>
+      letter === letter.toLowerCase() && !Number.isInteger(parseInt(letter))
+  );
+
+  const hasDigit = passwordArray.some((letter) => !isNaN(parseInt(letter)));
+
+  return hasDigit && hasLowerCaseLetter && hasUpperCaseLetter && hasValidLength;
+}
 
 module.exports = validatePassword;
